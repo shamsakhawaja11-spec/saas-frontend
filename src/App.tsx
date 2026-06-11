6eb66d7144370601a753a8d0bea4e14f53a84731
Shamsa Khawaja <shamsakhawaja11@gmail.com>
2026-06-11 19:32:21 +0500

    update: app routing with dashboard layout

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import LoginPage from './pages/auth/loginpage';
 import RegisterPage from './pages/auth/registerPage';
 import ProtectedRoute from './components/layout/protectedRoute';
+import DashboardLayout from './components/layout/Dashboardlayout';
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
+
         {/* Public routes */}
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
@@ -14,12 +16,54 @@ const App = () => {
         {/* Default redirect */}
         <Route path="/" element={<Navigate to="/login" replace />} />
 
-        {/* Protected routes — will add more pages here later */}
+        {/* Protected routes */}
         <Route path="/dashboard" element={
           <ProtectedRoute>
-            <div className="text-white text-center mt-20 text-2xl">
-              Dashboard — Coming Soon
-            </div>
+            <DashboardLayout>
+              <div className="text-white text-2xl font-bold">
+                Welcome to Dashboard 👋
+              </div>
+            </DashboardLayout>
+          </ProtectedRoute>
+        } />
+
+        <Route path="/workspaces" element={
+          <ProtectedRoute>
+            <DashboardLayout>
+              <div className="text-white text-2xl font-bold">
+                Workspaces — Coming Soon
+              </div>
+            </DashboardLayout>
+          </ProtectedRoute>
+        } />
+
+        <Route path="/projects" element={
+          <ProtectedRoute>
+            <DashboardLayout>
+              <div className="text-white text-2xl font-bold">
+                Projects — Coming Soon
+              </div>
+            </DashboardLayout>
+          </ProtectedRoute>
+        } />
+
+        <Route path="/boards" element={
+          <ProtectedRoute>
+            <DashboardLayout>
+              <div className="text-white text-2xl font-bold">
+                Boards — Coming Soon
+              </div>
+            </DashboardLayout>
+          </ProtectedRoute>
+        } />
+
+        <Route path="/tasks" element={
+          <ProtectedRoute>
+            <DashboardLayout>
+              <div className="text-white text-2xl font-bold">
+                Tasks — Coming Soon
+              </div>
+            </DashboardLayout>
           </ProtectedRoute>
         } />
 
