3f264a194cc79567060e455e5d4eca06acfdfb5f
Shamsa Khawaja <shamsakhawaja11@gmail.com>
2026-06-12 19:06:46 +0500

    feat: add workspaces api

---
+++ src/api/workspaces.api.ts
@@ -0,0 +1,18 @@
+import api from './axios';
+import type{ Workspace,CreateWorkspaceDto,UpdateWorkspaceDto } from '../types/workspace.types';
+
+export const createWorkspace=async(dto:CreateWorkspaceDto):Promise<Workspace>=>{
+    const response=await api.post<Workspace>('/workspaces',dto);
+    return response.data;
+}
+export const getWorkspaces=async():Promise<Workspace[]>=>{
+    const response=await api.get<Workspace[]>('/workspaces');
+    return response.data;
+}
+export const updateWorkspace=async(id:string,dto:UpdateWorkspaceDto):Promise<Workspace>=>{
+    const response=await api.put<Workspace>(`/workspaces/${id}`,dto);
+    return response.data;
+}
+export const deleteWorkspace=async(id:string):Promise<void>=>{
+    await api.delete<Workspace>(`/workspaces/${id}`);
+}
